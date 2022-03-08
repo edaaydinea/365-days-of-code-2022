9e91164dec406fcacaaee94a6b49cae2bafca94a
Eda AYDIN <edaaydin.ea@outlook.com>
2022-03-08 21:17:27 +0300

    Update EqualOrNotEqual.java

--- Algorithms/Part 1/01 - Fundamentals/01_1 - Basic Programming Model/Book/Exercises/EqualOrNotEqual.java
+++ Algorithms/Part 1/01 - Fundamentals/01_1 - Basic Programming Model/Book/Exercises/EqualOrNotEqual.java
@@ -1,30 +1,12 @@
-import java.util.Scanner;
+import edu.princeton.cs.algs4.StdOut;
 
 public class EqualOrNotEqual {
-    private static String equalOrNotEqual(int first_number, int second_number, int third_number) {
-
-        String firstnumber = String.valueOf(first_number);
-        String secondnumber = String.valueOf(second_number);
-        String thirdnumber = String.valueOf(third_number);
-
-        if (firstnumber.equals(secondnumber)) {
-            if (secondnumber.equals(thirdnumber)) {
-                return "equal";
-            }
-        } else
-            return "not equal";
-
-        return "No numbers founds";
-    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int first_number = scanner.nextInt();
-        int second_number = scanner.nextInt();
-        int third_number = scanner.nextInt();
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        int n3 = Integer.parseInt(args[2]);
 
-        String result = equalOrNotEqual(first_number, second_number, third_number);
-
-        System.out.println(result);
+        StdOut.println(n1 == n2 && n2 == n3 ? "equal" : "not equal");
     }
 }
