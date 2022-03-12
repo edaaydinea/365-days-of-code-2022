f72cc478cdb0711833d86f0b960d6de16433c13a
Eda AYDIN <edaaydin.ea@outlook.com>
2022-03-12 20:27:45 +0300

    Create GreatCircle.java

---
+++ Computer Science - An Interdisciplinary Approach/Book/1 - Elements of Programming/1.2 - Built-in Types of Data/Creative Exercises/GreatCircle.java
@@ -0,0 +1,13 @@
+public class GreatCircle {
+    public static void main(String[] args) {
+        double x1 = Math.toRadians(Double.parseDouble(args[0]));
+        double y1 = Math.toRadians(Double.parseDouble(args[1]));
+        double x2 = Math.toRadians(Double.parseDouble(args[2]));
+        double y2 = Math.toRadians(Double.parseDouble(args[3]));
+        double d = 60 * Math.acos(Math.sin(x1) * Math.sin(x2) +
+                Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
+
+        System.out.println("The great circle distance between Paris and San"
+                + " Francisco is " + Math.toDegrees(d) + " nautical miles");
+    }
+}
