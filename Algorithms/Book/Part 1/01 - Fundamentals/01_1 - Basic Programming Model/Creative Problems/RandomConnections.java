dedb1f0f9766b77dee34a41b52f50b4d8389f3a5
Eda AYDIN <edaaydin.ea@outlook.com>
2022-03-12 18:08:59 +0300

    Create RandomConnections.java

---
+++ Algorithms/Book/Part 1/01 - Fundamentals/01_1 - Basic Programming Model/Creative Problems/RandomConnections.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdDraw;
+
+public class RandomConnections {
+    public static void main(String[] args) {
+        StdDraw.circle(0.1, 0.1, 0.05);
+        StdDraw.circle(0.1, 0.2, 0.05);
+        StdDraw.setPenColor(StdDraw.GRAY);
+        StdDraw.line(0.1, 0.1, 0.1, 0.2);
+    }
+}
