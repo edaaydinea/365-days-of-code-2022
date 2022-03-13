f8816c13a7fd58d2884e9bc739108eb003f4bcff
Eda AYDIN <edaaydin.ea@outlook.com>
2022-03-13 16:59:35 +0300

    Create FlippingAFairCoin.java

---
+++ Computer Science - An Interdisciplinary Approach/Book/1 - Elements of Programming/1.3 - Conditionals and Loops/FlippingAFairCoin.java
@@ -0,0 +1,9 @@
+public class FlippingAFairCoin {
+    public static void main(String[] args) {
+        // Simulate a fair coin flip
+        if (Math.random() < 0.5)
+            System.out.println("Heads");
+        else
+            System.out.println("Tails");
+    }
+}
