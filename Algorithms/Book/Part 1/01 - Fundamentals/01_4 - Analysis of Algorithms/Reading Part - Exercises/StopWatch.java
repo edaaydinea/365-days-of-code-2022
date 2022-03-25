f0abb058234849ebaf12477f4d3631dde4201574
Eda AYDIN <edaaydin.ea@outlook.com>
2022-03-25 21:05:33 +0300

    Create StopWatch.java

---
+++ Algorithms/Book/Part 1/01 - Fundamentals/01_4 - Analysis of Algorithms/Reading Part - Exercises/StopWatch.java
@@ -0,0 +1,28 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.ThreeSum;
+
+public class StopWatch {
+    private final long start;
+
+    public StopWatch() {
+        start = System.currentTimeMillis();
+    }
+
+    public double elapsedTime() {
+        long now = System.currentTimeMillis();
+        return (now - start) / 1000.0;
+    }
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(-1000000, 1000000);
+        }
+        StopWatch timer = new StopWatch();
+        int cnt = ThreeSum.count(a);
+        double time = timer.elapsedTime();
+        StdOut.println(cnt + " triples " + time + " seconds");
+    }
+}
