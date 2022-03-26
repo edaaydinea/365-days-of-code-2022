ceb01844cc6a9aade2c935b109fad166355df77d
Eda AYDIN <edaaydin.ea@outlook.com>
2022-03-26 22:41:56 +0300

    Create poem.c

---
+++ C - C++ Programming/Section 1.1/poem.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Your brought me sunshine\n");
+    printf("when I only saw rain.\n");
+    printf("You brought me laughter\n");
+    printf("when I only felt pain.\n\n");
+
+    printf("Donna Donathan");
+}
