29f2ffdcd1d403b96f844c669dd1469ea8066474
Eda AYDIN <edaaydin.ea@outlook.com>
2022-04-03 18:55:08 +0300

    Create Exercise_16.java

---
+++ Algorithms/Book/Part 1/01 - Fundamentals/01_5 - Case Study -  Union - Find/Creative Exercises/Exercise_16.java
@@ -0,0 +1,105 @@
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.Random;
+
+public class Exercise_16 {
+    public static void main(String[] args) {
+        int n = 1000;
+        //StdDraw.setCanvasSize();
+        StdDraw.setXscale(0, n);
+        StdDraw.setYscale(0, n / 2);
+
+        Random r = new Random();
+        QuickUnionUF qu = new QuickUnionUF(n);
+        QuickFindUF qf = new QuickFindUF(n);
+        for (int i = 0; i < n; i++) {
+            int a = r.nextInt(n);
+            int b = r.nextInt(n);
+            qu.union(a, b);
+            qf.union(a, b);
+            StdDraw.setPenColor(0, 0, 200);
+            StdDraw.point(i, qu.access);
+            StdDraw.setPenColor(200, 0, 0);
+            StdDraw.point(i, qf.access);
+        }
+
+
+    }
+
+    public static class QuickUnionUF {
+        private final int[] id;
+        private int count;
+        private int access;
+
+        public QuickUnionUF(int n) {
+            id = new int[n];
+            for (int i = 0; i < n; i++) {
+                id[i] = i;
+            }
+            count = n;
+        }
+
+        public int find(int p) {
+            access++;         // initial while condition
+            while (p != id[p]) {
+                access++;     // assignment
+                p = id[p];
+                access++;     // loop while condition
+            }
+            return p;
+        }
+
+        public boolean connected(int p, int q) {
+            return find(p) == find(q);
+        }
+
+        public void union(int p, int q) {
+            access = 0;
+            p = find(p);
+            q = find(q);
+            if (p == q) return;
+            access++;     // assignment
+            id[q] = p;
+            count--;
+        }
+    }
+
+    public static class QuickFindUF {
+        private final int[] id;
+        private int count;
+        private int access;
+
+        public QuickFindUF(int n) {
+            id = new int[n];
+            for (int i = 0; i < n; i++) {
+                id[i] = i;
+            }
+            count = n;
+        }
+
+        public int find(int p) {
+            return id[p];
+        }
+
+        public boolean connected(int p, int q) {
+            return id[p] == id[q];
+        }
+
+        public void union(int p, int q) {
+            access = 0;
+            access++;
+            int idp = id[p];
+            access++;
+            int idq = id[q];
+            if (idp == idq) return;
+            for (int i = 0; i < id.length; i++) {
+                access++;
+                if (id[i] == idp) {
+                    access++;
+                    id[i] = idq;
+                }
+            }
+            count--;
+        }
+    }
+}
