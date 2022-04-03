9704035d43ecb58f0b5fbee3e2067433595a2399
Eda AYDIN <edaaydin.ea@outlook.com>
2022-04-03 18:55:46 +0300

    Create Exercise_08.java

---
+++ Algorithms/Book/Part 1/01 - Fundamentals/01_5 - Case Study -  Union - Find/Exercises/Exercise_08.java
@@ -0,0 +1,17 @@
+public class Exercise_08 {
+    public static void union(int p, int q) {
+        /*
+         * if (connected(p, q)) return;
+         * Rename p’s component to q’s name.
+         * for (int i = 0; i < id.length; i++)
+         *      if (id[i] == id[p]) id[i] = id[q];
+         * count--;
+         */
+    }
+
+    /*
+     * Answer:
+     *     union(0, 1);   // 1 1 2 3 4 5 ...
+     *     union(0, 2);   // 2 1 2 3 4 5 ...
+     */
+}
