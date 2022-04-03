4256ff10c6afdbefe5369cc412a05a1e602260a1
Eda AYDIN <edaaydin.ea@outlook.com>
2022-04-04 01:46:13 +0300

    Create Exercise_05.java

---
+++ Computer Science - An Interdisciplinary Approach/Book/1 - Elements of Programming/1.4 Arrays/Exercises/Exercise_05.java
@@ -0,0 +1,10 @@
+public class Exercise_05 {
+    public static void main(String[] args) {
+        // int[] a; -> java: variable a might not have been initialized
+        /*
+         * for (int i = 0; i < 10; i++) {
+         *  a[i] = i * i;
+         *  }
+         */
+    }
+}
