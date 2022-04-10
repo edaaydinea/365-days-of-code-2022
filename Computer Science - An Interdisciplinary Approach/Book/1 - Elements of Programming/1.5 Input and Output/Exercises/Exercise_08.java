c455e41a85c0dc855afcd11a749ba5f46e2e17f4
Eda AYDIN <46783629+edaaydinea@users.noreply.github.com>
2022-04-10 18:30:50 +0300

    Create Exercise_08.java

---
+++ Computer Science - An Interdisciplinary Approach/Book/1 - Elements of Programming/1.5 Input and Output/Exercises/Exercise_08.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+
+public class Exercise_08 {
+    public static void main(String[] args) {
+        StdOut.print("Enter positive floating-point numbers (ctrl + d to stop): ");
+
+        double geoMean;
+        double harmonicMean;
+        double sum = 0.0;
+
+        ArrayList<Double> numbers = new ArrayList<>();
+
+        while (!StdIn.isEmpty()) {
+            numbers.add(StdIn.readDouble());
+        }
+
+        int n = numbers.size(); // size of ArrayList
+
+        double multiplication = numbers.get(0);
+
+        for (Double number : numbers) {
+            multiplication *= number;
+        }
+
+        geoMean = (Math.pow(multiplication, 1.0 / n));
+        StdOut.println("Geometric mean: " + geoMean);
+
+        for (int j = 0; j < numbers.size(); j++) {
+            sum = 1.0 / numbers.get(j);
+        }
+
+        harmonicMean = n / sum;
+        StdOut.println("Harmonic mean: " + harmonicMean);
+    }
+}
