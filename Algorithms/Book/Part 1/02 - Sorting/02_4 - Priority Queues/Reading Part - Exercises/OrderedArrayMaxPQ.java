4ad77b55b4ed5e49b77b2c5c0e5e1feb0dbe90d9
Eda AYDIN <46783629+edaaydinea@users.noreply.github.com>
2022-04-11 23:09:11 +0300

    Create OrderedArrayMaxPQ.java

---
+++ Algorithms/Book/Part 1/02 - Sorting/02_4 - Priority Queues/Reading Part - Exercises/OrderedArrayMaxPQ.java
@@ -0,0 +1,50 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class OrderedArrayMaxPQ<Key extends Comparable<Key>> {
+    private final Key[] pq;          // elements
+    private int n;             // number of elements
+
+    // set inititial size of heap to hold size elements
+    public OrderedArrayMaxPQ(int capacity) {
+        pq = (Key[]) (new Comparable[capacity]);
+        n = 0;
+    }
+
+
+    public boolean isEmpty() { return n == 0;  }
+    public int size()        { return n;       }
+    public Key delMax()      { return pq[--n]; }
+
+    public void insert(Key key) {
+        int i = n-1;
+        while (i >= 0 && less(key, pq[i])) {
+            pq[i+1] = pq[i];
+            i--;
+        }
+        pq[i+1] = key;
+        n++;
+    }
+
+
+
+    /***************************************************************************
+     * Helper functions.
+     ***************************************************************************/
+    private boolean less(Key v, Key w) {
+        return v.compareTo(w) < 0;
+    }
+
+    /***************************************************************************
+     * Test routine.
+     ***************************************************************************/
+    public static void main(String[] args) {
+        OrderedArrayMaxPQ<String> pq = new OrderedArrayMaxPQ<>(10);
+        pq.insert("this");
+        pq.insert("is");
+        pq.insert("a");
+        pq.insert("test");
+        while (!pq.isEmpty())
+            StdOut.println(pq.delMax());
+    }
+
+}
