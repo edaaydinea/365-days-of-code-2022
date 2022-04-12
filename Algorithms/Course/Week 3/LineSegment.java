30479974de3afaa5f450f9eeefc7f782930fdd50
Eda AYDIN <46783629+edaaydinea@users.noreply.github.com>
2022-04-13 02:58:33 +0300

    Create LineSegment.java

---
+++ Algorithms/Course/Week 3/LineSegment.java
@@ -0,0 +1,59 @@
+/*************************************************************************
+ *  Compilation:  javac LineSegment.java
+ *  Execution:    none
+ *  Dependencies: Point.java
+ *
+ *  An immutable data type for Line segments in the plane.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ *  DO NOT MODIFY THIS CODE.
+ *
+ *************************************************************************/
+
+public record LineSegment(Point p, Point q) {
+    /**
+     * Initializes a new line segment.
+     *
+     * @param p one endpoint
+     * @param q the other endpoint
+     * @throws NullPointerException if either <tt>p</tt> or <tt>q</tt>
+     *                              is <tt>null</tt>
+     */
+    public LineSegment {
+        if (p == null || q == null) {
+            throw new NullPointerException("argument is null");
+        }
+    }
+
+
+    /**
+     * Draws this line segment to standard draw.
+     */
+    public void draw() {
+        p.drawTo(q);
+    }
+
+    /**
+     * Returns a string representation of this line segment
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this line segment
+     */
+    public String toString() {
+        return p + " -> " + q;
+    }
+
+    /**
+     * Throws an exception if called. The hashCode() method is not supported because
+     * hashing has not yet been introduced in this course. Moreover, hashing does not
+     * typically lead to good *worst-case* performance guarantees, as required on this
+     * assignment.
+     *
+     * @throws UnsupportedOperationException if called
+     */
+    public int hashCode() {
+        throw new UnsupportedOperationException();
+    }
+
+}
