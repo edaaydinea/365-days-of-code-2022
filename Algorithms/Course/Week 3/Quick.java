8b5620f3d9b26ae9b20196906d74ca6a5b8a8f87
Eda AYDIN <46783629+edaaydinea@users.noreply.github.com>
2022-04-14 01:39:05 +0300

    Create Quick.java

---
+++ Algorithms/Course/Week 3/Quick.java
@@ -0,0 +1,107 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Quick {
+    // partition the subarray a[lo..hi] so that a[lo..j-1] <= a[j] <= a[j+1..hi]
+    // and return the index j.
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        while (true) {
+            // find item on left to swap
+            while (less(a[++i], a[lo]))
+                if (i == hi) break;
+
+            // find item on right to swap
+            while (less(a[lo], a[--j]))
+                if (j == lo) break;
+
+            // check if pointers cross
+            if (i >= j) break;
+            // swap
+            exch(a, i, j);
+        }
+
+        // swap with partitioning item
+        exch(a, lo, hi);
+
+        // return index of item now known to be in place
+        return j;
+
+    }
+
+    // Rearranges the array in ascending order, using the natural order.
+    private static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        // shuffle needed for performance guarantee (stay tuned)
+        sort(a, 0, a.length - 1);
+    }
+
+    // quicksort the subarray from a[lo] to a[hi]
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        if (v == w) return false; // optimization when reference equals
+        return v.compareTo(w) < 0;
+    }
+
+    private static void show(Comparable[] a) {
+        for (Comparable comparable : a) {
+            StdOut.println(comparable);
+        }
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i - 1])) return false;
+        return true;
+    }
+
+    public static Comparable select(Comparable[] a, int k) {
+        if (k < 0 || k >= a.length) {
+            throw new IllegalArgumentException("index is not between 0 and " + a.length + ": " + k);
+        }
+        StdRandom.shuffle(a);
+        int lo = 0, hi = a.length - 1;
+        while (hi > lo) {
+            int i = partition(a, lo, hi);
+            if (i > k) hi = i - 1;
+            else if (i < k) lo = i + 1;
+            else return a[i];
+        }
+        return a[lo];
+    }
+
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        Quick.sort(a);
+        show(a);
+        assert isSorted(a);
+
+        // display results again using select
+        StdOut.println();
+        for (int i = 0; i < a.length; i++) {
+            String ith = (String) Quick.select(a, i);
+            StdOut.println(ith);
+        }
+
+    }
+
+
+}
