84efe22a22247ec0e9a11f49410fc68d3dc6a014
Eda AYDIN <46783629+edaaydinea@users.noreply.github.com>
2022-04-14 14:58:29 +0300

    Create TrinomialBrute.java

---
+++ Computer Science - An Interdisciplinary Approach/Course/Week 6/TrinomialBrute.java
@@ -0,0 +1,13 @@
+public class TrinomialBrute {
+    public static long trinomial(int n, int k) {
+        if (n == 0 && k == 0) return 1;
+        if (k < -n || k > n) return 0;
+        return trinomial(n - 1, k - 1) + trinomial(n - 1, k) + trinomial(n - 1, k + 1);
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[1]);
+        StdOut.println(trinomial(n, k));
+    }
+}
