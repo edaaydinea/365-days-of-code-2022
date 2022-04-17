f567c464280c46fe5c59fd1be57c4ac78b102330
Eda AYDIN <edaaydin.ea@outlook.com>
2022-04-17 17:13:31 +0300

    Create Exercise_14.java

---
+++ Algorithms/Book/Part 1/03 - Searching/03_1 - Symbol Tables/Exercises/Exercise_14.java
@@ -0,0 +1,22 @@
+public class Exercise_14 {
+    /*
+     *
+     *
+     *
+     *
+     *               algorithm               worst-case cost       average-case cost                 efficiently
+     *            (data structure)        (after N inserts)     (after N random inserts)          support ordered
+     *                                                                                               operations?
+     *                                   search       insert       searchhit        insert
+     *           sequential search
+     *          (unordered linked list)     N            N           N/2              N                  no
+     *
+     *            binary search
+     *            (ordered array)           lgN          2N          lgN              N                  yes
+     *
+     *
+     *
+     *
+     *
+     * */
+}
