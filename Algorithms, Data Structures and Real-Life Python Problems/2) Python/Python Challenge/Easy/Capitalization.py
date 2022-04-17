a87193e750a2214f98bd686af5351f1170c8177c
Eda AYDIN <edaaydin.ea@outlook.com>
2022-04-18 01:49:49 +0300

    Delete Capitalization.py

--- Algorithms, Data Structures and Real-Life Python Problems/2) Python/Python Challenge/Easy/Capitalization.py
+++
@@ -1,7 +0,0 @@
-def capitalization(sentence):
-    return " ".join(word.capitalize() for word in sentence.split())
-
-
-if __name__ == '__main__':
-    sentence = input("Enter the sentence: ")
-    print(capitalization(sentence))
