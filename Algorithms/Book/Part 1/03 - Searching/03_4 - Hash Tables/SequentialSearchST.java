d2f9ba7f2c638a229d6dd95dbb66138389df457b
Eda AYDIN <edaaydin.ea@outlook.com>
2022-04-19 14:56:08 +0300

    Create SequentialSearchST.java

---
+++ Algorithms/Book/Part 1/03 - Searching/03_4 - Hash Tables/SequentialSearchST.java
@@ -0,0 +1,175 @@
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/******************************************************************************
+ *  Compilation:  javac SequentialSearchST.java
+ *  Execution:    java SequentialSearchST
+ *  Dependencies: StdIn.java StdOut.java
+ *  Data files:   https://algs4.cs.princeton.edu/31elementary/tinyST.txt  
+ *
+ *  Symbol table implementation with sequential search in an
+ *  unordered linked list of key-value pairs.
+ *
+ *  % more tinyST.txt
+ *  S E A R C H E X A M P L E
+ *
+ *  % java SequentialSearchST < tiny.txt 
+ *  L 11
+ *  P 10
+ *  M 9
+ *  X 7
+ *  H 5
+ *  C 4
+ *  R 3
+ *  A 8
+ *  E 12
+ *  S 0
+ *
+ ******************************************************************************/
+
+
+public class SequentialSearchST<Key, Value> {
+    private int n;           // number of key-value pairs
+    private Node first;      // the linked list of key-value pairs
+
+    // a helper linked list data type
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node next;
+
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public SequentialSearchST() {
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Is this symbol table empty?
+     *
+     * @return {@code true} if this symbol table is empty and {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     *
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     * {@code false} otherwise
+     */
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the value associated with the given key.
+     *
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     * and {@code null} if the key is not in the symbol table
+     */
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key))
+                return x.val;
+        }
+        return null;
+    }
+
+    /**
+     * Inserts the key-value pair into the symbol table, overwriting the old value
+     * with the new value if the key is already in the symbol table.
+     * If the value is {@code null}, this effectively deletes the key from the symbol table.
+     *
+     * @param key the key
+     * @param val the value
+     */
+    public void put(Key key, Value val) {
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+        n++;
+    }
+
+    /**
+     * Removes the key and associated value from the symbol table
+     * (if the key is in the symbol table).
+     *
+     * @param key the key
+     */
+    public void delete(Key key) {
+        first = delete(first, key);
+    }
+
+    // delete key in linked list beginning at Node x
+    // warning: function call stack too large if table is large
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        if (key.equals(x.key)) {
+            n--;
+            return x.next;
+        }
+        x.next = delete(x.next, key);
+        return x;
+    }
+
+
+    /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return all keys in the symbol table as an {@code Iterable}
+     */
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<>();
+        for (Node x = first; x != null; x = x.next)
+            queue.enqueue(x.key);
+        return queue;
+    }
+
+
+    /**
+     * Unit tests the {@code SequentialSearchST} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        SequentialSearchST<String, Integer> st = new SequentialSearchST<>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+        for (String s : st.keys())
+            StdOut.println(s + " " + st.get(s));
+    }
+}
