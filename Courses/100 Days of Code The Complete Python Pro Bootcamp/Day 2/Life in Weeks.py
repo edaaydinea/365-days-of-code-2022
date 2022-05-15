e09259824b8f9a426a060530086221406c2dc3b8
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-15 19:54:22 +0300

    Update Life in Weeks.py

--- Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 2/Life in Weeks.py
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 2/Life in Weeks.py
@@ -2,10 +2,10 @@
 age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+# Write your code below this line 👇
 
 days = 90 * 365 - int(age) * 365
 weeks = 90 * 52 - int(age) * 52
 months = 90 * 12 - int(age) * 12
 
-print("You have {} days, {} weeks, and {} months left".format(days,weeks,months))
+print("You have {} days, {} weeks, and {} months left".format(days, weeks, months))
