c854b6ea7c41326cb4b8717d9ffc44b4b620bc71
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-15 19:54:19 +0300

    Create Tip Calculator.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 2/Tip Calculator.py
@@ -0,0 +1,20 @@
+# If the bill was $150.00, split between 5 people, with 12% tip.
+
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
+
+# Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+# Write your code below this line 👇
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? "))
+tip = int(input("How much tip would you like to give? 10, 12, or, 15? "))
+people = int(input("How many people to split the bill? "))
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+
+print("Each person should pay ${}".format(final_amount))
