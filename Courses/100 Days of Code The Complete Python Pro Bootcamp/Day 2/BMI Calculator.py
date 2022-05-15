3faf9fa6320dd239ebbe3fe06e3dcae36d462575
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-15 19:40:14 +0300

    Create BMI Calculator.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 2/BMI Calculator.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = int(weight) // (float(height) * float(height))
+print(int(bmi))
