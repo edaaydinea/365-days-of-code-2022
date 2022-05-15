622cb119b15f62181c434bf53be55e6d3da4f965
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-15 19:40:23 +0300

    Create Odd or Even.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 3/Odd or Even.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+if number > 0:
+    if number % 2 == 0:
+        print("This is an even number.")
+    else:
+        print("This is an odd number.")
+else:
+    print("Wrong number.")
