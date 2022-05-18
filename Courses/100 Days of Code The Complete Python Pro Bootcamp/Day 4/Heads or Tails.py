8baaa5601064b7899b2e1a8f2b7bf63318312234
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-18 13:53:22 +0300

    Create Heads or Tails.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 4/Heads or Tails.py
@@ -0,0 +1,15 @@
+# Remember to use the random module
+# Hint: Remember to import the random module here at the top of the file. 🎲
+import random
+
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+# 🚨 Don't change the code above 👆 It's only for testing your code.
+
+# Write the rest of your code below this line 👇
+random_number = random.randint(0, 1)
+if random_number == 1:
+    print("Heads")
+else:
+    print("Tails")
