0846252cd725c093260a8fbf4300ee3ff56e0423
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-18 19:23:36 +0300

    Create FirstProgram.java

---
+++ Courses/The Complete Java Development Bootcamp/Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -0,0 +1,15 @@
+// TASK 1: Make a class.
+public class FirstProgram {
+    // TASK 2: Inside the class, write the main() method.
+    public static void main(String[] args) {
+        // TASK 3: Print these messages to the console: "Printing is fun!"
+        // "Java > Python."
+        // "I spilled Java all over my paper."
+        // "My dog ate my Java."
+
+        System.out.println("Printing is fun!\nJava > Python\nI spilled Java all over my paper.\nMy dog ate my Java");
+    }
+}
+
+
+
