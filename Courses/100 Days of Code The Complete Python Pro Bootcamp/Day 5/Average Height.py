b79c73294739fbb73fffe2305e9ba5b43eb870a6
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-18 14:55:58 +0300

    Create Average Height.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 5/Average Height.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+# Write your code below this row 👇
+total_height = 0
+
+for height in student_heights:
+    total_height += height
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+
+average_height = round(total_height / number_of_students)
+print(average_height)
