2d0618b80fab0bf02efff73c6ef8f4638ce8522f
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-18 15:04:57 +0300

    Delete all assignments of previous courses

--- Courses/MATLAB Programming/Section 1/Section 1.2/MATLAB Calculator.m
+++
@@ -1,4 +0,0 @@
-% We borrowed $1000 at a 10% annual interest rate.
-% If we did not make a payment for two years, and assuming there is no penalty for non-payment, how much do we owe now?
-% Assign the result to a variable called debt.
-debt = 1000 + 1000 * 0.1 + (1000 + 1000 * 0.1) * 0.1
