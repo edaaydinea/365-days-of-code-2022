02cfbcc47760404aae995cf3c82d6c17944368b3
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-19 13:26:21 +0300

    Create Hurdle 2.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 6/Hurdle 2.py
@@ -0,0 +1,19 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    jump()
