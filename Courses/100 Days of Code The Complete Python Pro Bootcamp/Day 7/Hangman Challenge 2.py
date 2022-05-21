506ee5f4cadc9c72b78e7778f4c42fb00491c10a
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-21 15:32:41 +0300

    Create Hangman Challenge 2.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 7/Hangman Challenge 2.py
@@ -0,0 +1,31 @@
+# Step 2
+
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+# TODO-1: - Create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display.append("_")
+
+guess = input("Guess a letter: ").lower()
+
+# TODO-2: - Loop through each position in the chosen_word;
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+
+# TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
