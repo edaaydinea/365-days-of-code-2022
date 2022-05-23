cf3afb07ffff63efd6a48881427c3610868c355c
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-24 00:53:29 +0300

    Create YourInitials.java

---
+++ Courses/The Complete Java Development Bootcamp/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here.
+        System.out.println(" SSS     A  ");
+        System.out.println("S   S   A A ");
+        System.out.println("S      A   A");
+        System.out.println(" SSS   AAAAA");
+        System.out.println("    S  A   A");
+        System.out.println("S   S  A   A");
+        System.out.println(" SSS   A   A");
+    }
+}
