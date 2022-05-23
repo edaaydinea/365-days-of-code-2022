224665aebc42f3e6042aa76721db7ac1fbbbfc19
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-23 13:19:09 +0300

    Create Prime Numbers.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 8/Prime Numbers.py
@@ -0,0 +1,20 @@
+# Write your code below this line 👇
+def prime_checker(number):
+    flag = False
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                flag = True
+                break
+
+    if flag:
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")
+
+
+# Write your code above this line 👆
+
+# Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
