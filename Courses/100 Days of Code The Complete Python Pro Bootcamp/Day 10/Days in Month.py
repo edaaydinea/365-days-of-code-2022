d13e10a35a3023be72c8689a30f84ce36fe10c6e
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-24 15:03:03 +0300

    Create Days in Month.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 10/Days in Month.py
@@ -0,0 +1,27 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year.")
+            else:
+                print("Not leap year.")
+        else:
+            print("Leap year.")
+    else:
+        print("Not leap year.")
+
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if month > 12 or month < 1:
+        return "Invalid month entered"
+    if month == 2 and is_leap(year):
+        return 29
+    return month_days[month - 1]
+
+
+# 🚨 Do NOT change any of the code below
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
