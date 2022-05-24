3cf1d218f0584d1e6cd99c38be9834d2c8163a8e
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-24 15:02:46 +0300

    Create Day 10.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 10/Day 10.py
@@ -0,0 +1,37 @@
+# %%
+# Functions with Outputs
+def format_name(f_name, l_name):
+    """
+    Take a first and last name and format it to return the title case version of the name.
+    :param f_name: first name taken from user as string
+    :param l_name: last name taken from user as string
+    :return: formatted first name and last name
+    """
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    f"Result: {formated_f_name} {formated_l_name}"
+
+
+# %%
+# Storing output in a variable
+formatted_name = format_name(input("Your first name: "), input("Your last name: "))
+print(formatted_name)
+# or printing output directly
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
+# Already used functions with outputs.
+length = len(formatted_name)
+
+
+# %%
+# Return as an early exit
+def format_name(f_name, l_name):
+    """Take a first and last name and format it
+    to return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
