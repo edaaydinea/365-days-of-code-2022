4c37a8afbcdd0f686f5cfa23851753f761bd865c
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-25 18:51:00 +0300

    Update Chatbot.java

--- Courses/The Complete Java Development Bootcamp/Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
+++ Courses/The Complete Java Development Bootcamp/Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
@@ -1,37 +1,28 @@
-//import Scanner
+import java.util.Scanner;
 
 public class Chatbot {
     public static void main(String[] args) {
-        
-        //set up scanner. 
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello. What is your name?");
-        //Pick up user's name and store it. 
+        String name = scanner.nextLine();
 
-                           
-        //add new a line before asking next question. 
-        System.out.println("Hi <name>! I'm Javabot. Where are you from?");
-        //Pick up user's home and store it. 
-                        
+        System.out.println("\nHi " + name + "! I'm Javabot. Where are you from?");
+        String home = scanner.nextLine();
 
-        //add new a line before asking next question.
-        System.out.println("I hear it's beautiful at <home>! I'm from a place called Oracle");
+        System.out.println("\nI hear it's beautiful at " + home + "! I'm from a place called Oracle");
         System.out.println("How old are you?");
-        //Pick up age and store it.
+        int age = scanner.nextInt();
 
-
-        //add new a line before asking next question.
-        System.out.println("So you're <age>, cool! I'm 400 years old.");
-        System.out.println("This means I'm <divide 400 by age> times older than you.");
+        System.out.println("\nSo you're " + age + ", cool! I'm 400 years old.");
+        System.out.println("This means I'm " + 400 / age + " times older than you.");
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
-        //Pick up language and store it. 
+        scanner.nextLine();
+        String language = scanner.nextLine();
 
+        System.out.println("\n" + language + ", that's great! Nice chatting with you " + name + ". I have to log off now. See ya!");
 
-        //add new a line here.
-        System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
-        
-        //close scanner. 
-
-        
+        scanner.close();
     }
 }
