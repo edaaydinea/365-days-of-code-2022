0418c39003cbcb09e649d6bc055ed91660d9d51c
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-29 18:07:42 +0300

    Create Dictionary in List.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 9/Dictionary in List.py
@@ -0,0 +1,29 @@
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+
+
+# 🚨 Do NOT change the code above
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 👇
+def add_new_country(country_name, visit_count, cities_visited):
+    new_country = {}
+    new_country["country"] = country_name
+    new_country["visits"] = visit_count
+    new_country["cities"] = cities_visited
+    travel_log.append(new_country)
+
+
+# 🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
