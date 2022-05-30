3395f893a16efbc2bf6030cb8c7f118943e5a206
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-30 19:01:52 +0300

    Create menu.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 15/menu.py
@@ -0,0 +1,25 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "coffee": 24,
+            "milk": 150
+        },
+        "cost": 2.5,
+    },
+    "cappucino": {
+        "ingredients": {
+            "water": 250,
+            "coffee": 24,
+            "milk": 100
+        },
+        "cost": 3.0,
+    }
+}
