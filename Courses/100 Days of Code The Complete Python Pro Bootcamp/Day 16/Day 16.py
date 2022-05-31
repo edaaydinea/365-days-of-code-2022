2256def8ecaee6086fa1aed4f1b7daaf9a986dcb
Eda AYDIN <edaaydin.ea@outlook.com>
2022-05-31 12:58:18 +0300

    Create Day 16.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 16/Day 16.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+# %%
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+
+print(table)
