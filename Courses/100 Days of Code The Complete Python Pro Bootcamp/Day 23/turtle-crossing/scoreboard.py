4a24a821db6d6aa39b38f0451bc79718ae17dd2f
Eda AYDIN <edaaydin.ea@outlook.com>
2022-06-30 16:31:55 +0300

    Create scoreboard.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 23/turtle-crossing/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+FONT = ("Courier", 16, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write("Level: {}".format(self.level), align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
