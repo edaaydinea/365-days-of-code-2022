759952cae1384ba7b8285aa2cfdd5462d3333ce8
Eda AYDIN <edaaydin.ea@outlook.com>
2022-07-01 19:50:09 +0300

    Update scoreboard.py (Add the reading and writing part from the txt file)

--- Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 24/snake_game/scoreboard.py
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 24/snake_game/scoreboard.py
@@ -8,7 +8,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -24,6 +25,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
