1ad45ca5fb7adb8bfd2e7f421a2270d549bc1954
Eda AYDIN <edaaydin.ea@outlook.com>
2022-12-15 02:59:19 +0300

    Create main1.py

---
+++ Courses/100 Days of Code The Complete Python Pro Bootcamp/Day 25/main1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv("Squirrel_Data.csv")
+
+# Get all the necessary information
+df_new1 = df.value_counts(["Primary Fur Color"], sort=True).reset_index(name="Count")
+
+# Change the column name
+df_new1.rename(columns={"Primary Fur Color": "Fur Color"}, inplace=True)
+df_new1.to_csv("squirrel_count.csv")
